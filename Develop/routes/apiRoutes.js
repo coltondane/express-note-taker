326b0ea65d6ef7b948970079764988932c46df83
coltondane <113216904+coltondane@users.noreply.github.com>
2023-09-27 16:53:25 -0600

    fixed post bug and implemented delete function

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,31 @@
-// imports 
-const router = require('express').Router();
-const fs = require('fs');
-const db = require('../db/db.json')
+// // imports 
+// const router = require('express').Router();
+// const fs = require('fs');
+// const db = require('../db/db.json')
+// const path = require('path');
 
-// get the notes api from the database and send send it to the client
-// localhost:3001/api/notes
-router.get('/notes', (req, res) => {
-    // read db file
-    fs.readFileSync(db, 'utf8', (err, data) => {
-        if (err) throw err;
-        // send back the data response to the client
-        res.json(data)
-    })
-})
+// // get the notes api from the database and send send it to the client
+// // localhost:3001/api/notes
+// router.get('/notes', (req, res) => {
+//     // read db file
+//     // fs.readFileSync(db, 'utf8', (err, data) => {
+//     //     if (err) throw err;
+//     //     // send back the data response to the client
+//     // })
+//     res.json(db)
+// })
 
-// export
-module.exports = router
+// // write to the file with post
+// router.post('/notes', (req, res) => {
+//     // read db file
+//     const newArray = db;
+//     newArray.push(req.body)
+//     console.log(db);
+//     fs.writeFile(db, newArray, (err) => {
+//         if (err) throw err;
+//         // send back the data response to the client
+//         res.status(200);
+//     })
+// })
+// // export
+// module.exports = router
--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+// uuid 
+const { v4: uuidv4 } = require('uuid');
+const db = require('./db/db.json');
 
 
 // initializations variables
@@ -18,7 +22,66 @@ app.use(express.static('public'));
 
 // point to the routes folder
 // localhost:3001/api
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
+
+app.get('/api/notes', (req, res) => {
+    // read db file
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        // send back the data response to the client
+        res.json(JSON.parse(data))
+    });
+    res.status(200);
+});
+
+// write to the file with post
+app.post('/api/notes', (req, res) => {
+    // read db file
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        // push req.body (what the user enters) into the data
+        const {title, text} = req.body
+        if (title && text) {
+            const newObject = {
+                text,
+                title,
+                id: uuidv4(),
+            }
+            const array = JSON.parse(data);
+            array.push(newObject);
+            fs.writeFile('./db/db.json', JSON.stringify(array), (err) => {
+                if (err) throw err;
+                // send back the data response to the client
+                console.log("test", array);
+                res.json(array);
+            });
+        };
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    // gather the data
+    console.log(req.params.id);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        // parse the data
+        const array = JSON.parse(data);
+        // use the .filter method to return a new array by looping through and filtering out anything that matches the conditional, so if the id doesnt match, we want it to go into the new array
+        for (let i = 0; i < array.length; i++) {
+            const note = array[i];
+            if (note.id === req.params.id) {
+                array.splice(i, 1);
+            }
+        }
+        console.log(array);
+        // write the new array without the selected object to the file
+        fs.writeFile('./db/db.json', JSON.stringify(array), (err) => {
+            if (err) throw err;
+            res.json(array);
+        })
+    })
+
+})
+
 // localhost:3001/ will be the home page
 app.use('/', htmlRoutes);
 
